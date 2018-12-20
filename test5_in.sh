fb88e7f237e87c5b7202a65d0e77aabd6cadb555
Shimizu Tamiko <tamiko.shimizu@PC-1174.local>
2018-12-20 18:44:09 +0900

    fix test5 file

--- test5_in.sh
+++ test5_in.sh
@@ -1,1 +1,5 @@
-curl -X POST "http://localhost:8000/v1.0/estimate/genre" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d 'tmc_session_id=test5-genre-1tweet&utterances="Windowsに限らず他のOSでも、ハードウェアの故障、モデム、ルーター等の問題は原因不明って出るらしいけど…。 ＰＣにケーブル差さってないは論外"' | jq .
+POST
+curl -X POST "http://localhost:8000/v1.0/estimate/genre" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d 'tmc_session_id=test-1&utterances="私は中華料理が好きです。"'
+
+GET
+curl -X GET "http://localhost:8000/v1.0/estimate/genre?job_session_id=c38d194f-f317-4785-a0b3-e02ee81694fb" -H "accept: application/json"
